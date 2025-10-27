ae151c99ca1376697ea2e9b1d402a49aba13a03b
Patrick <woodfordpuddie@gmail.com>
2025-10-27 07:47:33 +0000

    Front End Prelim

--- project/libsysinfo/usbInfo.cc
+++ project/libsysinfo/usbInfo.cc
@@ -1,6 +1,7 @@
 /*
  *  USB information class.  Executes lsusb and parses output into arrays of buses and
- *  devices.  Parsing is now robust and does not rely on fixed substring positions.
+ *  devices.  Parsing is crude and just expects particular fields to be at particular
+ *  locations in the output.
  *
  *  Copyright (c) 2024 Mark Burkley (mark.burkley@ul.ie)
  */
@@ -28,14 +29,13 @@ struct USBDevice
 struct USBBus
 {
     static const int maxDevice = 32;
-    int deviceCount = 0;
+    int deviceCount;
     USBDevice device[maxDevice];
 };
 
 class USBInfo
 {
 public:
-    USBInfo() : _busCount(0) {}
     void read();
     int busCount () { return _busCount; }
     int deviceCount (int bus) { return _bus[bus].deviceCount; }
@@ -47,57 +47,69 @@ private:
     int _busCount;
     USBBus _bus[maxBus];
     void _parseDevice (char buffer[]);
-    int _xtoi (const string& s, int idefault);
+    int _xtoi (string& s, int idefault);
 };
 
 USBInfo usb;
 
-// Convert string to int, safely handles hex (with or without 0x)
-int USBInfo::_xtoi(const string& s, int idefault) {
+int USBInfo::_xtoi (string& s, int idefault)
+{
+    int ival;
+
     try {
-        size_t idx;
-        int ival = stoi(s, &idx, 16);
-        return ival;
-    } catch (...) {
-        return idefault;
+        ival = stoi (s, 0, 16);
     }
+    catch(exception &err) {
+        ival = idefault;
+    }
+
+    return ival;
 }
 
-// Safe parser for lsusb output
 void USBInfo::_parseDevice(char buffer[]) {
     string line = buffer;
     istringstream iss(line);
+    string tmp, busStr, deviceStr, vidPid;
 
-    string tmp, busStr, deviceStr, idStr;
-    if (!(iss >> tmp >> busStr >> tmp >> deviceStr >> tmp >> idStr)) {
-        // Line does not match expected format
-        return;
-    }
+    // Attempt to read the main tokens
+    if (!(iss >> tmp >> busStr >> tmp >> deviceStr >> tmp >> vidPid))
+        return; // malformed line, ignore
 
-    // Remove possible trailing colon from device number
+    // Remove trailing colon from device number
     if (!deviceStr.empty() && deviceStr.back() == ':')
         deviceStr.pop_back();
 
-    int bus = stoi(busStr, nullptr, 10);
-    int device = stoi(deviceStr, nullptr, 10);
+    // Safely parse bus and device numbers
+    int bus = 0, device = 0;
+    try {
+        bus = stoi(busStr);
+        device = stoi(deviceStr);
+    } catch (...) {
+        return; // ignore malformed numbers
+    }
 
-    auto pos = idStr.find(':');
-    if (pos == string::npos)
-        return; // unexpected ID format
+    // Check that vidPid contains ':' before splitting
+    auto pos = vidPid.find(':');
+    if (pos == string::npos || pos + 1 >= vidPid.size())
+        return; // malformed ID, ignore
 
-    string vendorStr = idStr.substr(0, pos);
-    string productStr = idStr.substr(pos + 1);
+    string vendorStr = vidPid.substr(0, pos);
+    string productStr = vidPid.substr(pos + 1);
 
+    // Assign vendor/product safely
     _bus[bus].device[device].vendor = _xtoi(vendorStr, 0);
     _bus[bus].device[device].product = _xtoi(productStr, 0);
 
+    // Update counts
     if (bus > _busCount)
         _busCount = bus;
     if (device > _bus[bus].deviceCount)
         _bus[bus].deviceCount = device;
 }
 
-void USBInfo::read() {
+
+void USBInfo::read()
+{
     std::array<char, 4096> buffer;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("lsusb", "r"), pclose);
 
@@ -106,26 +118,26 @@ void USBInfo::read() {
     }
 
     while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr)
-        _parseDevice(buffer.data());
+        _parseDevice (buffer.data());
 }
 
-// JNI wrappers
-JNIEXPORT void JNICALL Java_usbInfo_read(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_usbInfo_read (JNIEnv *env, jobject obj) {
     usb.read();
 }
 
-JNIEXPORT jint JNICALL Java_usbInfo_busCount(JNIEnv *env, jobject obj) {
-    return usb.busCount();
+JNIEXPORT jint JNICALL Java_usbInfo_busCount (JNIEnv *env, jobject obj) {
+    return usb.busCount ();
 }
 
-JNIEXPORT jint JNICALL Java_usbInfo_deviceCount(JNIEnv *env, jobject obj, jint bus) {
-    return usb.deviceCount(bus);
+JNIEXPORT jint JNICALL Java_usbInfo_deviceCount (JNIEnv *env, jobject obj, jint bus) {
+    return usb.deviceCount (bus);
 }
 
-JNIEXPORT jint JNICALL Java_usbInfo_vendorID(JNIEnv *env, jobject obj, jint bus, jint device) {
-    return usb.vendorID(bus, device);
+JNIEXPORT jint JNICALL Java_usbInfo_vendorID (JNIEnv *env, jobject obj, jint bus, jint device) {
+    return usb.vendorID (bus, device);
 }
 
-JNIEXPORT jint JNICALL Java_usbInfo_productID(JNIEnv *env, jobject obj, jint bus, jint device) {
-    return usb.productID(bus, device);
+JNIEXPORT jint JNICALL Java_usbInfo_productID (JNIEnv *env, jobject obj, jint bus, jint device) {
+    return usb.productID (bus, device);
 }
+
